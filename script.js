be3175c558433f2cd0720edcec299b76c07425bc
husseinabduljebar7-de <husseinabduljebar7@gmail.com>
2026-01-17 10:53:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Simple animation on scroll
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("shadow");
+    } else {
+        navbar.classList.remove("shadow");
+    }
+});
